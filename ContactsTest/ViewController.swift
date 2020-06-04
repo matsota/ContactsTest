4c85d52d44ab683cc8711100a1c2eda17f07eb87
Matsota Andrew <matsotaandrew@icloud.com>
2020-06-04 12:19:31 +0300

    Initial Commit

---
+++ ContactsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsTest
+//
+//  Created by Andrew Matsota on 04.06.2020.
+//  Copyright © 2020 Andrew Matsota. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
